2c86ae0abc6bd8500c7139c0152915720dd5c205
Gregory Smith <greg@Gregsi7MacMini.fios-router.home>
2023-01-24 11:25:26 -0500

    update

--- monitor.sh
+++ monitor.sh
@@ -1,9 +1,9 @@
 while true; do
-	fast --json > fast.jnk
+	./node_modules/.bin/fast --json > fast.jnk
 	cat fast.jnk
 	speed=`jq < fast.jnk ".downloadSpeed"`
 	date=`date "+%Y-%m-%d,%H:%M:%S"`
 	epoch=$(date -j -f "%a %b %d %T %Z %Y" "`date`" "+%s")
 	echo "$epoch,$date,$speed" | tee -a fast.csv
-	sleep 60
+	sleep 900 # 15 minutes
 done
