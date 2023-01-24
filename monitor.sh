8341f68140dafb8a6d57c103b040027c87361e31
Greg Smith <thinkable@thinkable.us>
2023-01-24 11:09:04 -0500

    update

--- monitor.sh
+++ monitor.sh
@@ -1,6 +1,9 @@
-#fast --json > fast.jnk
-#speed=`jq < fast.jnk ".downloadSpeed"`
-speed=100
-date=`date "+%Y-%m-%d,%H:%M:%S"`
-epoch=$(date -j -f "%a %b %d %T %Z %Y" "`date`" "+%s")
-echo "$epoch,$date,$speed" >> fast.csv
+while true; do
+	fast --json > fast.jnk
+	cat fast.jnk
+	speed=`jq < fast.jnk ".downloadSpeed"`
+	date=`date "+%Y-%m-%d,%H:%M:%S"`
+	epoch=$(date -j -f "%a %b %d %T %Z %Y" "`date`" "+%s")
+	echo "$epoch,$date,$speed" | tee -a fast.csv
+	sleep 60
+done
